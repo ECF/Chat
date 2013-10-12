8a3c57dc9e677f6af1c2efd02968c289403a6ec4
Markus Alexander Kuppe <github.com@lemmster.de>
2013-10-12 13:58:50 +0200

    Show messages into a TableViewer instead of a text field (allows for easier styling of table items) #2

---
+++ org.eclipse.ecf.example.chat.product/src/org/eclipse/ecf/example/chat/product/parts/ChatElement.java
@@ -0,0 +1,47 @@
+package org.eclipse.ecf.example.chat.product.parts;
+
+import java.util.Date;
+
+public class ChatElement {
+
+	private final String message;
+	private final Date date;
+	private final boolean isLocal;
+	private final String handle;
+
+	public ChatElement(String message, String handle, Date date, boolean isLocal) {
+		this.message = message;
+		this.handle = handle;
+		this.date = date;
+		this.isLocal = isLocal;
+	}
+
+	/**
+	 * @return the user
+	 */
+	public String getHandle() {
+		return handle;
+	}
+
+	/**
+	 * @return the service
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @return the date
+	 */
+	public Date getDate() {
+		return date;
+	}
+
+	/**
+	 * @return the isLocal
+	 */
+	public boolean isLocal() {
+		return isLocal;
+	}
+
+}

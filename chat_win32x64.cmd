61eca134fd46e78768ac2ada7689a9f9e91583e6
Wim Jongman <wim.jongman@gmail.com>
2013-10-13 15:53:16 +0200

    Update chat_win32x64.cmd

--- chat_win32x64.cmd
+++ chat_win32x64.cmd
@@ -1,1 +1,1 @@
-./org.eclipse.ecf.example.chat.product\target\products\org.eclipse.ecf.example.chat.product\win32\win32\x86_64\chat\chatclient.exe
+org.eclipse.ecf.example.chat.product\target\products\org.eclipse.ecf.example.chat.product\win32\win32\x86_64\chat\chatclient.exe

2015b8f30723b67b53ab8d9c25f29cffc875d22c
Markus Alexander Kuppe <github.com@lemmster.de>
2013-10-24 18:51:26 +0200

    Adapt ChatServer startup script (the one that does not need launcher) to the mvn/tycho build

--- org.eclipse.ecf.example.chat.server.feature/ChatServer.sh
+++ org.eclipse.ecf.example.chat.server.feature/ChatServer.sh
@@ -6,6 +6,9 @@ test -e plugins/org.eclipse.ecf.server.generic_*.jar && rm plugins/org.eclipse.e
 test -e plugins/org.eclipse.ecf.remoteservice.servlet_*.jar && rm plugins/org.eclipse.ecf.remoteservice.servlet_*.jar
 
 test -e plugins/org.eclipse.osgi_*.jar && mv plugins/org.eclipse.osgi_*.jar $BASEDIR
-test -e plugins/org.eclipse.equinox.simpleconfigurator_*.jar && mv plugins/org.eclipse.equinox.simpleconfigurator_*.jar $BASEDIR
+
+# rewrites the location of the bundles in plugins/ folder
+# (This does not seem necessary when building with PDE export)
+sed -i 's/reference\\:file\\:/reference\\:file\\:plugins\//g' configuration/config.ini
 
 java -Dzoodiscovery.flavor=zoodiscovery.flavor.centralized=disco.ecf-project.org -Dzoodiscovery.clientPort=2181 -Dzoodiscovery.consoleLog -Dzoodiscovery.autoStart -Dch.ethz.iks.r_osgi.topic.filter=org/eclipse/e4/* -Declipse.ignoreApp=true -Dosgi.noShutdown=true -jar $BASEDIR/org.eclipse.osgi_*.jar -console -consoleLog 

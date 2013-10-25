94eb140802a3b86d4f06b8e5fb01416d40288a20
Markus Alexander Kuppe <github.com@lemmster.de>
2013-10-25 09:14:32 +0200

    Mirror system properties into product

--- org.eclipse.ecf.example.chat.server.feature/ChatServer.sh
+++ org.eclipse.ecf.example.chat.server.feature/ChatServer.sh
@@ -11,4 +11,5 @@ test -e plugins/org.eclipse.osgi_*.jar && mv plugins/org.eclipse.osgi_*.jar $BAS
 # (This does not seem necessary when building with PDE export)
 sed -i 's/reference\\:file\\:/reference\\:file\\:plugins\//g' configuration/config.ini
 
+# Update properties or parameters in /org.eclipse.ecf.example.chat.server.product/org.eclipse.ecf.example.chat.server.product accordingly
 java -Dzoodiscovery.flavor=zoodiscovery.flavor.centralized=disco.ecf-project.org -Dzoodiscovery.clientPort=2181 -Dzoodiscovery.consoleLog -Dzoodiscovery.autoStart -Dch.ethz.iks.r_osgi.topic.filter=org/eclipse/e4/* -Declipse.ignoreApp=true -Dosgi.noShutdown=true -jar $BASEDIR/org.eclipse.osgi_*.jar -console -consoleLog 

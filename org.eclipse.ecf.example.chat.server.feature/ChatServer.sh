8ea399351bb01a46b2240aaec7cdf5c16c172453
Markus Alexander Kuppe <github.com@lemmster.de>
2013-11-04 18:07:09 +0100

    Add Felix webconsole to server build

--- org.eclipse.ecf.example.chat.server.feature/ChatServer.sh
+++ org.eclipse.ecf.example.chat.server.feature/ChatServer.sh
@@ -12,4 +12,4 @@ test -e plugins/org.eclipse.osgi_*.jar && mv plugins/org.eclipse.osgi_*.jar $BAS
 sed -i 's/reference\\:file\\:/reference\\:file\\:plugins\//g' configuration/config.ini
 
 # Update properties or parameters in /org.eclipse.ecf.example.chat.server.product/org.eclipse.ecf.example.chat.server.product accordingly
-java -Dzoodiscovery.flavor=zoodiscovery.flavor.centralized=disco.ecf-project.org -Dzoodiscovery.clientPort=2181 -Dzoodiscovery.consoleLog -Dzoodiscovery.autoStart -Dch.ethz.iks.r_osgi.topic.filter=org/eclipse/e4/* -Declipse.ignoreApp=true -Dosgi.noShutdown=true -jar $BASEDIR/org.eclipse.osgi_*.jar -console -consoleLog 
+java -Dorg.osgi.service.http.port=9980 -Dzoodiscovery.flavor=zoodiscovery.flavor.centralized=disco.ecf-project.org -Dzoodiscovery.clientPort=2181 -Dzoodiscovery.consoleLog -Dzoodiscovery.autoStart -Dch.ethz.iks.r_osgi.topic.filter=org/eclipse/e4/* -Declipse.ignoreApp=true -Dosgi.noShutdown=true -jar $BASEDIR/org.eclipse.osgi_*.jar -console -consoleLog 
